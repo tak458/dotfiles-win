8fb9f952f2658f66cf4104bc327e1427303a8549
tak458 <takah458@gmail.com>
2020-10-13 02:06:43 +0900

    パスの移動を修正

--- WindowsPowerShell/Modules/GitExtension/GitExtension.psm1
+++ WindowsPowerShell/Modules/GitExtension/GitExtension.psm1
@@ -1,7 +1,10 @@
 function Remove-GitBranchMerged {
-    Set-Location $Args[0]
+    Push-Location $Args[0]
+
     git branch --merged `
         | Select-String -NotMatch -Pattern "(\*|develop|master)" `
         | ForEach-Object{ git branch -d $_.ToString().Trim() }
     git fetch --prune origin
+
+    Pop-Location
 }
