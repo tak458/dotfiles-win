fdd4aef68181164a8c2056b9b3add885ca34c7fe
tak458 <takah458@gmail.com>
2021-06-07 20:41:33 +0900

    add remove-dotenv

--- PowerShell/Modules/DotEnvExtension/DotEnvExtension.psm1
+++ PowerShell/Modules/DotEnvExtension/DotEnvExtension.psm1
@@ -11,3 +11,18 @@ function Import-DotEnv {
         Write-Host "Loaded `$env:$key='$value'"
     }
 }
+
+# perge .env file
+function Remove-DotEnv {
+    Get-ChildItem (Get-Location) -File ".env" |
+    ForEach-Object { 
+        Write-Host "Loading $_"
+        Get-Content $_
+    } |
+    ForEach-Object {
+        $key, $value = $_.split('=', 2);
+        Remove-Item "env:$key"
+        Write-Host "Removed `$env:$key"
+    }
+
+}
